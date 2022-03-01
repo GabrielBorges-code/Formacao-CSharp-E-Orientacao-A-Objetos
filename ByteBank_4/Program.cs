09280158754fea77eb9549f3b76a1724e897afeb
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-03-01 03:38:38 -0300

    iniciando a parte 4 do curso C#

---
+++ ByteBank_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using ByteBank_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente1 = new ContaCorrente(331, 433);
+
+            int div = 10 / 20;
+
+            Console.WriteLine(div);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using ByteBank_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Erro de referência");
+            }
+            
+            Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possivel dividir por zero");
+            }
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            //ContaCorrente conta = null;
+            //Console.WriteLine(conta.Saldo);
+
+            return numero / divisor;
+        }
+    }
+}
+
