c4e82534f7f3cc3f4bb864d374be92364833b1d7
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-03-12 06:46:50 -0300

    iniciando a parte 5 do curso de C#

---
+++ ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();
+            //teste.MetodoPrivado(); //inacessível devido ao seu nível de proteção
+            //teste.MetodoProtegido(); //inacessível devido ao seu nível de proteção
+            teste.MetodoInterno();
+
+        }
+        
+    }
+
+    class Derivada : ModificadoresTeste
+    {
+        public Derivada()
+        {
+          MetodoPublico();
+          //MetodoPrivado(); //inacessível devido ao seu nível de proteção
+          MetodoProtegido();
+          MetodoInterno();
+
+        }
+    }
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            //Visível fora da classe "ModificadoresTeste"
+        }
+        private void MetodoPrivado()
+        {
+            //Visível apenas na classe "ModificadoresTeste"
+        }
+
+        protected void MetodoProtegido()
+        {
+            //Visível apenas na classe "ModificadoresTeste" e derivados
+
+        }
+        
+        internal void MetodoInterno()
+        {
+            //Visível apenas para o projeto ByteBank.Modelos!
+        }
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(963, 1234);
+        
+            Console.WriteLine(conta1.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
