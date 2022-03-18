f563d915e3add1a6177bf32560131a6ae55e32b6
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-03-18 02:14:15 -0300

    C# parte 6 finalizado

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,19 +14,41 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string endereco = "R. São Carlos do Pinhal nº746";
-            string enderecoFormatado =
-                endereco
-                    .ToUpper()
-                    .Replace("r.", "Rua")
-                    .Replace(" nº", ", Número ")
-                    .ToUpper();
+            Cliente cliente1 = new Cliente();
+            cliente1.Nome = "Carlos Roberto";
+            cliente1.CPF = "587.985.654-20";
+            cliente1.Profissao = "Developer";
 
-            Console.WriteLine(enderecoFormatado);
+            Cliente cliente2 = new Cliente();
+            cliente2.Nome = "Carlos Roberto";
+            cliente2.CPF = "587.985.654-20";
+            cliente2.Profissao = "Developer";
+
+            Console.WriteLine(cliente1 == cliente2);
 
             Console.ReadLine();
-        }
 
-       
+            // string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            // string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            // string padrao = "[0-9]{4,5}[-][0-9]{4}";
+            // string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            // string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoDeTeste = "Meu nome é Gabriel Borges, meu telefone (61) 98888-4444.";
+                
+            string padraoCPF = "[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}";
+            string textoDeTeste2 = "Meu CPF é 065.987.123-12";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+            Match resultadoCPF = Regex.Match(textoDeTeste2, padraoCPF);
+
+            Console.WriteLine("Telefone: " + resultado);
+            Console.WriteLine("CPF: " + resultadoCPF);
+
+            Console.WriteLine($"escrevendo variavel textoDeTeste2: \n{textoDeTeste2}");
+
+            Console.ReadLine();
+
+        }
     }
 }
