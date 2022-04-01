0ce06983bbf8f469a4090057848f974c3057f913
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-03-31 22:58:38 -0300

    C# parte 7 finalizada

---
+++ P07ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,115 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07ByteBank.SistemaAgencia
+{
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+
+        public ListaDeContaCorrente(int capacidadeInicial = 5) // 5 vira um valor inicial e padrão de 5
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+        public int Tamanho { get { return _proximaPosicao; } }
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+            //Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+
+            _itens[_proximaPosicao] = item; 
+            _proximaPosicao++;
+        }
+        public void AdicionarVarios(params ContaCorrente[] itens) //params para receber mais de uma paramêtro
+        {
+            foreach (ContaCorrente conta in itens)
+            {
+                Adicionar(conta);
+            }
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            //Console.WriteLine("Aumentando a capacidade da lista");
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                novoArray[i] = _itens[i];
+                //Console.WriteLine("interando");
+            }
+
+            _itens = novoArray;
+        }
+
+        public void Remover(ContaCorrente item)
+        {
+            int indiceItem = -1;
+            
+            for (int i =0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente itemAtual = _itens[i];
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+            for (int i = indiceItem; i < _proximaPosicao-1; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+            _itens[_proximaPosicao] = null;
+
+        }
+       
+        public ContaCorrente GetItemNoIndice(int indice)
+        {
+            if (indice < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        public ContaCorrente this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+
+        /*public void EscreverListaNaTela()
+        {
+            for(int i = 0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente conta = _itens[i];    
+                Console.WriteLine($"{i} - Conta número: {conta.Numero}, Conta Agência: {conta.Agencia}");
+            }
+        }*/
+    }
+}
--- P07ByteBank.SistemaAgencia/Program.cs
+++ P07ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+//using ByteBank.SistemaAgencia;
 
 namespace P07ByteBank.SistemaAgencia
 {
@@ -12,8 +13,106 @@ namespace P07ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+
+            Lista<int> idade = new Lista<int>();
+
+            idade.Adicionar(7);
+            idade.Adicionar(4);
+            idade.Adicionar(10);
+            idade.AdicionarVarios(11, 15, 3, 8);
+
+            int idadeSoma = 0;
+            for (int i = 0; i < idade.Tamanho; i++)
+            {
+                int idadeAtual = idade[i];
+                idadeSoma += idadeAtual;    
+                Console.WriteLine(idadeAtual);
+            }
+
+            int idadeMedia = idadeSoma / idade.Tamanho;
+
+            Console.WriteLine($"idade total {idadeSoma}, idade média {idadeMedia}");
+
+            Console.ReadLine();
+        }
+
+        static void TestaListaDeObject()
+        {
+            ListaDeObject listaDeIdades = new ListaDeObject();
+
+            listaDeIdades.Adicionar(10);
+            listaDeIdades.Adicionar(7);
+            listaDeIdades.Adicionar(8);
+            listaDeIdades.Adicionar(8);
+            listaDeIdades.AdicionarVarios(10, 16, 23, 15, 60);
+
+            for (int i = 0; i < listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+
+                Console.WriteLine($"Idade no índice {i}: {idade}");
+            }
+
+        }
+        static void TestaListaDeContaCorrente()
+        {
+
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contadoGui = new ContaCorrente(9999999, 222222);
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                contadoGui,
+                new ContaCorrente(9874, 36984),
+                new ContaCorrente(9873, 87464)
+            };
+
+            lista.AdicionarVarios(contas);
+            lista.AdicionarVarios(
+                contadoGui,
+                new ContaCorrente(9875, 98723),
+                new ContaCorrente(8875, 96321),
+                new ContaCorrente(7453, 98874)
+                );
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i];
+                Console.WriteLine($"Item na posição {i} = Conta {itemAtual.Numero} e {itemAtual.Agencia}");
+            }
+
+
+        }
+        static int SomarVarios(params int[] numeros)
+        {
+            int acumulador = 0;
+            foreach (int numero in numeros)
+            {
+                acumulador += numero;
+            }
+            return acumulador;
+        }
+        static void TestaContaArray()
+        {
+           ContaCorrente[] contas = new ContaCorrente[]
+           {
+               new ContaCorrente(3218, 8765),
+               new ContaCorrente(3217, 9874),
+               new ContaCorrente(3216, 5412)
+           };
+
+
+            for (int i = 0; i < contas.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} - Nº da conta: {contas[i].Numero}, Agência da conta: {contas[i].Agencia}");
+            }
+        }
+
+        static void VariosArrays()
+        {
             int[] idades = new int[5];
-            
+
             idades[0] = 30;
             idades[1] = 24;
             idades[2] = 55;
@@ -22,11 +121,11 @@ namespace P07ByteBank.SistemaAgencia
 
             int recebeValor = idades[1];
             int somaIdades = 0;
-            
+
             for (int i = 0; i < idades.Length; i++)
             {
                 Console.WriteLine($"indice: {i} - {idades[i]} anos de idade");
-                somaIdades += idades[i]; 
+                somaIdades += idades[i];
             }
 
             int mediaIdades = somaIdades / idades.Length;
@@ -46,24 +145,6 @@ namespace P07ByteBank.SistemaAgencia
             {
                 Console.WriteLine($"indice: {i} - {arrayDeIdades[i]} anos de idade");
 
-            }
-
-            Console.ReadLine();
-        }
-
-        public static void outroArray()
-        {
-           ContaCorrente[] contas = new ContaCorrente[]
-           {
-               new ContaCorrente(3218, 8765),
-               new ContaCorrente(3217, 9874),
-               new ContaCorrente(3216, 5412)
-           };
-
-
-            for (int i = 0; i < contas.Length; i++)
-            {
-                Console.WriteLine($"{i + 1} - Nº da conta: {contas[i].Numero}, Agência da conta: {contas[i].Agencia}");
             }
         }
     }
